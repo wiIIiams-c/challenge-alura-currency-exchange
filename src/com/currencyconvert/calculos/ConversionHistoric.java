eb91eb5a187126ecfbdc6b4ae792aeee1984993c
Williams C <wilcontrerasf@gmail.com>
2024-04-19 22:10:01 -0400

    Create ConversionHistoric
    Clase que genera archivo con todas las conversiones que se han realizado asi como tambien para leer el archivo creado

---
+++ src/com/currencyconvert/calculos/ConversionHistoric.java
@@ -0,0 +1,34 @@
+package com.currencyconvert.calculos;
+
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class ConversionHistoric {
+    private String fileName = "Conversion-Historic.txt";
+    
+    public void saveFileHistoricConversion(String dataToSave){
+        String data = dataToSave;
+
+        try {
+            FileWriter fw = new FileWriter(fileName, true);
+            fw.write(data + "\n");
+            fw.close();
+        } catch (Exception e) {
+            System.out.println("Error al guardar en el archivo: " + e.getMessage());
+        }
+    }
+
+    public void readFileHistoricConversion(){
+        try {
+            List<String> listHistoric = Files.readAllLines(Paths.get(fileName));
+            
+            for(String list: listHistoric){
+                System.out.println(list);
+            }
+        } catch (Exception e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+}
