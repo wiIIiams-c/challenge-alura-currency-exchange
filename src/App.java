1fbcd1ef6a7306800ffe0b24f9ee70dab5464d67
Williams C <wilcontrerasf@gmail.com>
2024-04-20 22:08:33 -0400

    Update App
    Se mueve a un try-catch para ver si ingresan una opcion invalida
    
    Se cambia el .nextLine por .nextInt

--- src/App.java
+++ src/App.java
@@ -14,47 +14,52 @@ public class App {
         while (inOption != 3) {
             menu.optionHeader();
 
-            ConversionHistoric conversionHistoric = new ConversionHistoric();
-            System.out.println("\nEscriba la opcion deseada:");
-            Scanner inputOption = new Scanner(System.in);
-            inOption = Integer.valueOf(inputOption.nextLine());
-
-            if(inOption == 1){
-                menu.optionMenu();
-                
-                Scanner inputCurrency = new Scanner(System.in);
-                System.out.println("Ingresa opcion de la divisa de origen:");
-                var inCurrency = menu.getCurrencyCodeByNumberOption(Integer.valueOf(inputCurrency.nextLine()));
-        
-                Scanner outputCurrency = new Scanner(System.in);
-                System.out.println("\nIngresa opcion de la divisa a convertir:");
-                var outCurrency = menu.getCurrencyCodeByNumberOption(Integer.valueOf(outputCurrency.nextLine()));
-
-                Scanner inputValue = new Scanner(System.in);
-                System.out.println("\nEscriba el valor a convertir:");
-                var inValue = Double.valueOf(inputValue.nextLine());
-
-                Currency currency = new Currency(inCurrency, outCurrency, inValue);
-                CalculateConversion calculateConversion = new CalculateConversion();
-
-                calculateConversion.setConversionResult(currency);
-                conversionHistoric.saveFileHistoricConversion(currency.toString());
-                
-                System.out.println("\n******************************");
-                System.out.println("Informacion de conversion de divisa:\n");
-                System.out.println(currency.toString());
-                System.out.println("******************************\n");
-                
-            }else if(inOption == 2){
-                System.out.println("\n******************************");
-                System.out.println("Historial de Conversiones:\n");
-                conversionHistoric.readFileHistoricConversion();
-                System.out.println("******************************\n");
-            }else if(inOption == 3){
-                System.exit(0);
-            }else{
-                System.out.println("No es una opcion valida");
+            try {
+                ConversionHistoric conversionHistoric = new ConversionHistoric();
+                System.out.println("\nEscriba la opcion deseada:");
+                Scanner inputOption = new Scanner(System.in);
+                inOption = inputOption.nextInt();
+    
+                if(inOption == 1){
+                    menu.optionMenu();
+                    
+                    Scanner inputCurrency = new Scanner(System.in);
+                    System.out.println("Ingresa opcion de la divisa de origen:");    
+                    var inCurrency = menu.getCurrencyCodeByNumberOption(inputCurrency.nextInt());
+            
+                    Scanner outputCurrency = new Scanner(System.in);
+                    System.out.println("\nIngresa opcion de la divisa a convertir:");
+                    var outCurrency = menu.getCurrencyCodeByNumberOption(outputCurrency.nextInt());
+    
+                    Scanner inputValue = new Scanner(System.in);
+                    System.out.println("\nEscriba el valor a convertir:");
+                    var inValue = Long.valueOf(inputValue.nextLine());
+    
+                    Currency currency = new Currency(inCurrency, outCurrency, inValue);
+                    CalculateConversion calculateConversion = new CalculateConversion();
+    
+                    calculateConversion.setConversionResult(currency);
+                    conversionHistoric.saveFileHistoricConversion(currency.toString());
+                    
+                    System.out.println("\n******************************");
+                    System.out.println("Informacion de conversion de divisa:\n");
+                    System.out.println(currency.toString());
+                    System.out.println("******************************\n");
+                    
+                }else if(inOption == 2){
+                    System.out.println("\n******************************");
+                    System.out.println("Historial de Conversiones:\n");
+                    conversionHistoric.readFileHistoricConversion();
+                    System.out.println("******************************\n");
+                }else if(inOption == 3){
+                    System.exit(0);
+                }else{
+                    System.out.println("\nNo es una opcion valida\n");
+                }
+            } catch (Exception e) {
+                System.out.println("\nNo es una opcion valida\n");
             }
+
         }
     }
 }
